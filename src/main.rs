3221e5a1403b6d5e285cb1dd9ec59b4db472d26b
Alexei Ozerov <alexei.ozerov.7@gmail.com>
2021-01-14 11:32:23 -0500

    Added code to accomodate for a jsonline partway through a line of stdout

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,7 @@ fn deserialize_stdin() -> Result<Vec<String>, Box<dyn std::error::Error>> {
 
     for line in stdin.lines() {
         let l_res = line.unwrap();
-        let l_start = &l_res[..1];
-
-        // Check For JSON
-        if l_start == "{" {
-            log_vec.push(l_res.to_string().clone());
-        } else {
-            println!(
-                "\n{}Warning, non-JSON Log Found:{}\n{}",
-                color::Fg(color::Yellow),
-                color::Fg(color::Reset),
-                &l_res
-            );
-        }
+        log_vec.push(l_res.to_string().clone());
     }
 
     Ok(log_vec)
@@ -48,31 +36,82 @@ fn main() {
     };
 
     // Determine Output
+    // TODO: Break up common code into functions ...
     for s in &res {
-        let v: Value = serde_json::from_str(&s).unwrap();
-
-        if v["level"] == matches.value_of("LEVEL").unwrap() {
-            if let Some(f) = matches.value_of("FIELDS") {
-                let comma_delimit = f.split(",");
-                let fields_vec: Vec<&str> = comma_delimit.collect();
-                println!("\n");
-                for e in fields_vec {
+        if &s[..1] == "{" {
+            let v: Value = serde_json::from_str(&s).unwrap();
+            if v["level"] == matches.value_of("LEVEL").unwrap() {
+                if let Some(f) = matches.value_of("FIELDS") {
+                    let comma_delimit = f.split(",");
+                    let fields_vec: Vec<&str> = comma_delimit.collect();
+                    println!("\n");
+                    for e in fields_vec {
+                        println!(
+                            "{}{}:{} {},",
+                            color::Fg(color::Red),
+                            e,
+                            color::Fg(color::Reset),
+                            v[e],
+                        );
+                    }
                     println!(
-                        "{}{}:{} {},",
+                        "{}timestamp:{} {}",
                         color::Fg(color::Red),
-                        e,
                         color::Fg(color::Reset),
-                        v[e],
+                        v["timestamp"]
                     );
+                } else {
+                    println!("\n{}", v);
                 }
-                println!(
-                    "{}timestamp:{} {}",
-                    color::Fg(color::Red),
-                    color::Fg(color::Reset),
-                    v["timestamp"]
-                );
+            }
+        } else {
+            if s.contains("{\"") {
+                let split = s.split("{\"");
+                let split_vec: Vec<&str> = split.collect();
+                for n in 0..split_vec.len() {
+                    if split_vec[n].contains("\"}") {
+                        let json_line = "{\"".to_owned() + &split_vec[n];
+                        let v: Value = serde_json::from_str(&json_line).unwrap();
+                        if v["level"] == matches.value_of("LEVEL").unwrap() {
+                            if let Some(f) = matches.value_of("FIELDS") {
+                                let comma_delimit = f.split(",");
+                                let fields_vec: Vec<&str> = comma_delimit.collect();
+                                println!("\n");
+                                for e in fields_vec {
+                                    println!(
+                                        "{}{}:{} {},",
+                                        color::Fg(color::Red),
+                                        e,
+                                        color::Fg(color::Reset),
+                                        v[e],
+                                    );
+                                }
+                                println!(
+                                    "{}timestamp:{} {}",
+                                    color::Fg(color::Red),
+                                    color::Fg(color::Reset),
+                                    v["timestamp"]
+                                );
+                            } else {
+                                println!("\n{}", v);
+                            }
+                        }
+                    } else {
+                        println!(
+                            "\n{}Warning, non-JSON Log Found:{}\n{}",
+                            color::Fg(color::Yellow),
+                            color::Fg(color::Reset),
+                            split_vec[n]
+                        );
+                    }
+                }
             } else {
-                println!("\n{}", v);
+                println!(
+                    "\n{}Warning, non-JSON Log Found:{}\n{}",
+                    color::Fg(color::Yellow),
+                    color::Fg(color::Reset),
+                    &s
+                );
             }
         }
     }
